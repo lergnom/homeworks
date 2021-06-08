2d0a8a5a52e8557bd427119a785ae6cea0fcf46c
lergnom <lergnom@mail.ru>
2021-06-08 17:27:31 +0300

    add geader.mule.css

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom'
+import s from './Header.module.css'
 
 function Header() {
     return (
         <div>
-            // add NavLinks
+
+            <NavLink className={s.navLink} to={'/pre-junior'} activeClassName={s.link}>pre-junior</NavLink>
+            <NavLink className={s.navLink} to={'/junior'} activeClassName={s.link}>junior</NavLink>
+            <NavLink className={s.navLink} to={'/junior+'} activeClassName={s.link}>junior+</NavLink>
+
+            <span>links</span>
+
 
         </div>
     )
